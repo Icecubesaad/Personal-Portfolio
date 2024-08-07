e004656318b75342a9ce5da23565bdde58b79952
Icecubesaad <saad.qad418@gmail.com>
2024-08-07 22:33:31 +0500

    updated experience

--- src/app/experience/page.jsx
+++ src/app/experience/page.jsx
@@ -3,14 +3,14 @@ const experience=()=>{
         <div className="w-[80%] pl-24 pt-14 flex flex-col overflow-y-scroll custom-scrollbar pb-20 ">
             <h1 className="text_fancy3">Experience</h1>
             <article>
-            <h2 className="text_fancy5 mt-3">Frontend Developer (August-2023-Present)</h2>
-            <h3 className="text_fancy6 mt-3">Company : Ittefaq Electronic</h3>
+            <h2 className="text_fancy5 mt-3">Fullstack Developer (August-2023-Present)</h2>
+            <h3 className="text_fancy6 mt-3">Company : Uplift</h3>
             <p className="text_fancy4 mt-3">
-            During my tenure at Ittefaq Electronics as a FrontEnd Developer, I had the privilege to contribute to the company&aposs digital transformation and enhance its online presence. I played a pivotal role in developing and maintaining the front end of thier website and web applications, ensuring a seamless user experience for their customers
+            As a Full Stack Developer at Uplift, I collaborated with a talented team to develop robust mobile and web applications. My key responsibilities included ensuring smooth and efficient data flow between the frontend and backend, optimizing performance and reliability. I designed and implemented reusable and scalable UI components, enhancing user experience and maintainability. Additionally, I developed and integrated secure authentication systems, safeguarding user data and maintaining privacy.
             </p>
             </article>
             <article className=" mt-5">
-            <h2 className="text_fancy5 mt-5">Full-stack Developer (May-2022-present)</h2>
+            <h2 className="text_fancy5 mt-5">Full-stack Developer (June-2022-present)</h2>
             <h3 className="text_fancy6 mt-5">Platform : Upword, Out-source</h3>
             <p className="text_fancy4 mt-3">
             During my freelancing journey as a full-stack developer, one standout project involved collaborating with a startup to develop an e-commerce platform. From frontend design with React.js to backend development with Node.js and MongoDB, I navigated various technologies to deliver a seamless solution. Regular communication with the client ensured alignment with their vision, resulting in a successful project that underscored the dynamic and rewarding nature of freelancing in full-stack development.
