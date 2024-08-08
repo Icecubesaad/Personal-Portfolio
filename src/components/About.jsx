ce0ac615e26282b1ac264630565b7f383b42bf3e
Icecubesaad <saad.qad418@gmail.com>
2024-08-09 03:32:24 +0500

    ...

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,10 @@
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
+
 const About = () => {
   const [responsive, setresponsive] = useState(100);
+
   useEffect(() => {
     handleResize();
     function handleResize() {
@@ -19,17 +21,17 @@ const About = () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
   return (
     <div id="about" className="w-full pl-24 pt-14 flex flex-col overflow-y-scroll custom-scrollbar pb-20 responsive_c">
       <h1 className="nunito text-4xl font-[700]">About</h1>
       <div className="w-full nunito font-[600] text-[#c8d1f0] text-xl reponsive_ab text-justify">
-      <span className=" mt-2 block leading-[2rem]">
-  Hey there 👋 <br/> 
-  <span className=" block mt-2">
-    Since 2020, I have been developing my abilities as a motivated programmer with a solid background in web development. My primary focus is on the MERN stack, although I am proficient in a wide range of technologies, including React, Next.js, Express, and Node.js. Currently, I am pursuing a bachelor's degree in computer science at the University of Central Punjab, where I am constantly researching new technologies. As a self-taught programmer, I have undertaken numerous projects, showcasing my ability to learn independently and implement innovative solutions. Additionally, I have gained practical experience by working as a freelancer and collaborating effectively within teams, including my role at Ittefaq Electronics and Uplift where I contributed in building web and mobile applications. I absolutely love and thrive in the aspect of constantly learning and pondering on challenging problems. My passion for coding and commitment to lifelong learning motivate me to look for fresh opportunities and challenges in the web development industry. I'm excited to network with like-minded people and provide my expertise to worthwhile causes.
-  </span>
-</span>
-
+        <span className=" mt-2 block leading-[2rem]">
+          Hey there 👋 <br /> 
+          <span className=" block mt-2">
+            Since 2020, I have been developing my abilities as a motivated programmer with a solid background in web development. My primary focus is on the MERN stack, although I am proficient in a wide range of technologies, including React, Next.js, Express, and Node.js. Currently, I am pursuing a bachelor&#39;s degree in computer science at the University of Central Punjab, where I am constantly researching new technologies. As a self-taught programmer, I have undertaken numerous projects, showcasing my ability to learn independently and implement innovative solutions. Additionally, I have gained practical experience by working as a freelancer and collaborating effectively within teams, including my role at Ittefaq Electronics and Uplift where I contributed in building web and mobile applications. I absolutely love and thrive in the aspect of constantly learning and pondering on challenging problems. My passion for coding and commitment to lifelong learning motivate me to look for fresh opportunities and challenges in the web development industry. I&#39;m excited to network with like-minded people and provide my expertise to worthwhile causes.
+          </span>
+        </span>
       </div>
       <h1 className="nunito text-3xl font-[700]  mt-6">FrontEnd Stack</h1>
       <div className="flex flex-row lg:gap-20 gap-5 justify-center mt-5">
@@ -197,4 +199,5 @@ const About = () => {
     </div>
   );
 };
+
 export default About;
